ff4c90478f7f3e51f03a1392cab4b6942e4b24d1
Anjan <anjan7201@gmail.com>
2023-07-09 21:01:13 +0530

    Automax

---
+++ src/main/consts.py
@@ -0,0 +1,22 @@
+CAR_BRANDS = (
+    ('bmw', 'BMW'),
+    ('mercedes benz', 'Mercedes Benz'),
+    ('audi', 'Audi'),
+    ('tata nexon', 'Tata Nexon'),
+    ('tesla', 'Tesla'),
+    ('jaguar', 'Jaguar'),
+    ('land rover', 'Land Rover'),
+    ('bentley', 'Bentley'),
+    ('bugatti', 'Bugatti'),
+    ('ferrari', 'Ferrari'),
+    ('lamborghini', 'Lamborghini'),
+    ('honda', 'Honda'),
+    ('toyota', 'Toyota'),
+    ('swift dzire', 'Swift Dzire'),
+    ('porsche', 'Porsche')
+)
+
+TRANSMISSION_OPTIONS = (
+    ('automatic', 'Automatic'),
+    ('manual', 'Manual'),
+)
